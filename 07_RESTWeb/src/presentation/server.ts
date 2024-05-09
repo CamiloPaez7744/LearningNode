c8db1f7dddadff58671c1801914f22249d2c381c
CamiloPaez <camiloapaez@ucundinamarca.edu.co>
2024-05-09 09:21:32 -0500

    Create rest todo API

--- 07_RESTWeb/src/presentation/server.ts
+++ 07_RESTWeb/src/presentation/server.ts
@@ -1,8 +1,10 @@
-import express from 'express';
+import express, { type Router } from 'express';
+import path from 'path';
 
 interface Options {
     port: number;
     public_path?: string;
+    routes: Router;
 }
 
 export class Server {
@@ -10,27 +12,41 @@ export class Server {
     private app = express();
     private readonly port: number;
     private readonly publicPath: string;
+    private readonly routes: Router;
 
     constructor( options: Options) {
-        const { port, public_path } = options;
+        const { port, public_path, routes } = options;
         this.port = port;
         this.publicPath = public_path ?? 'public';
+        this.routes = routes;
     }
 
     async start() {
 
         //* Middleware
+        this.app.use(express.json());
+        this.app.use(express.urlencoded({ extended: true }));
 
         this.app.use(express.static(this.publicPath));
 
+        //* Routes
+
+        this.app.use( this.routes );
+
+
         this.app.get('*', (req, res) => {
-            console.log(req.url);
-            res.sendFile('index.html', { root: this.publicPath });
+            const indexPath = path.join( __dirname + `../../../${ this.publicPath }/index.html` );
+            res.sendFile(indexPath);
         });
+
+        // this.app.get('*', (req, res) => {
+        //     console.log(req.url);
+        //     res.sendFile('index.html', { root: this.publicPath });
+        // });
 
 
         this.app.listen(this.port, () => {
-            console.log(`Server is running on http://localhost:${this.port}`);
+            console.log(`Server is running on port ${this.port}`);
         });
     }
 }
